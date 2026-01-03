a4df9ba4371237ecd42e0393726e613b484f7771
EmmanuelleB985 <emmanuelle.bourigault@dtc.ox.ac.uk>
2026-01-03 14:22:22 +0000

    updates

---
+++ src/evaluation/__init__.py
@@ -0,0 +1,12 @@
+"""Evaluation utilities for FinDiffusion."""
+
+from .stylized_facts import StylizedFactsValidator, validate_stylized_facts
+from .metrics import compute_all_metrics, distribution_metrics, temporal_metrics
+
+__all__ = [
+    "StylizedFactsValidator",
+    "validate_stylized_facts",
+    "compute_all_metrics",
+    "distribution_metrics",
+    "temporal_metrics",
+]
